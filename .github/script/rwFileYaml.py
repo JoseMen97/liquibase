2727dbbe47d73d6e0883ff5ef0df870385093544
JoseMen97 <69913439+JoseMen97@users.noreply.github.com>
2024-01-16 21:30:11 -0500

    Update rwFileYaml.py

--- .github/script/rwFileYaml.py
+++ .github/script/rwFileYaml.py
@@ -1,20 +1,26 @@
 import yaml
 import sys
 
-# Ruta del archivo YAML
-archivo_yaml = sys.argv[1]
+with open('output.txt', 'r') as file:
+    # Itera sobre cada línea del archivo
+    for line in file:
+        # Imprime cada línea
+        print(line.strip())
+        
+# # Ruta del archivo YAML
+# archivo_yaml = sys.argv[1]
 
-# Leer el contenido del archivo YAML
-with open(archivo_yaml, 'r') as file:
-    contenido = yaml.safe_load(file)
+# # Leer el contenido del archivo YAML
+# with open(archivo_yaml, 'r') as file:
+#     contenido = yaml.safe_load(file)
 
-# Modificar la línea específica
-contenido['databaseChangeLog'][0]['changeSet']['id'] = sys.argv[2]
-contenido['databaseChangeLog'][0]['changeSet']['author'] = sys.argv[3]
-contenido['databaseChangeLog'][0]['changeSet']['changes'][0]['sqlFile']['path'] = sys.argv[4]
+# # Modificar la línea específica
+# contenido['databaseChangeLog'][0]['changeSet']['id'] = sys.argv[2]
+# contenido['databaseChangeLog'][0]['changeSet']['author'] = sys.argv[3]
+# contenido['databaseChangeLog'][0]['changeSet']['changes'][0]['sqlFile']['path'] = sys.argv[4]
 
-# contenido[sys.argv[2]] = sys.argv[3]
+# # contenido[sys.argv[2]] = sys.argv[3]
 
-# Escribir el contenido modificado de vuelta al archivo YAML
-with open(archivo_yaml, 'w') as file:
-    yaml.dump(contenido, file, default_flow_style=False)
+# # Escribir el contenido modificado de vuelta al archivo YAML
+# with open(archivo_yaml, 'w') as file:
+#     yaml.dump(contenido, file, default_flow_style=False)
