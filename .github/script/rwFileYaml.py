59a3675c25f9226d6f48134d8ca8f7d9ed96f755
JoseMen97 <69913439+JoseMen97@users.noreply.github.com>
2024-01-19 11:22:28 -0500

    Update rwFileYaml.py

--- .github/script/rwFileYaml.py
+++ .github/script/rwFileYaml.py
@@ -4,44 +4,49 @@ import shutil
 
 archivo_origen = 'changeset_base.yaml'
 
-with open('output.txt', 'r') as file:
-    # Itera sobre cada línea del archivo
-    for line in file:
-        # Imprime cada línea
-        partes = line.strip().split("/")
-        name = partes[-1]
-        archivo_destino = f'{name}_changeset_new.yaml'
-        shutil.copy(archivo_origen, archivo_destino)
+# with open('output.txt', 'r') as file:
+#     # Itera sobre cada línea del archivo
+#     for line in file:
+#         # Imprime cada línea
+#         partes = line.strip().split("/")
+#         name = partes[-1]
+#         archivo_destino = f'{name}_changeset_new.yaml'
+#         shutil.copy(archivo_origen, archivo_destino)
 
-        archivo_yaml = archivo_destino
+#         archivo_yaml = archivo_destino
 
-        # Leer el contenido del archivo YAML
-        with open(archivo_yaml, 'r') as file:
-            contenido = yaml.safe_load(file)
+#         # Leer el contenido del archivo YAML
+#         with open(archivo_yaml, 'r') as file:
+#             contenido = yaml.safe_load(file)
 
-        # Modificar la línea específica
-        contenido['databaseChangeLog'][0]['changeSet']['id'] = sys.argv[1]
-        contenido['databaseChangeLog'][0]['changeSet']['author'] = sys.argv[2]
-        path = f'../{line.strip()}'
-        contenido['databaseChangeLog'][0]['changeSet']['changes'][0]['sqlFile']['path'] = path
+#         # Modificar la línea específica
+#         contenido['databaseChangeLog'][0]['changeSet']['id'] = sys.argv[1]
+#         contenido['databaseChangeLog'][0]['changeSet']['author'] = sys.argv[2]
+#         path = f'../{line.strip()}'
+#         contenido['databaseChangeLog'][0]['changeSet']['changes'][0]['sqlFile']['path'] = path
         
-        with open(archivo_yaml, 'w') as file:
-            yaml.dump(contenido, file, default_flow_style=False)
-        
-# # Ruta del archivo YAML
-# archivo_yaml = sys.argv[1]
+#         with open(archivo_yaml, 'w') as file:
+#             yaml.dump(contenido, file, default_flow_style=False)
 
-# # Leer el contenido del archivo YAML
-# with open(archivo_yaml, 'r') as file:
-#     contenido = yaml.safe_load(file)
 
-# # Modificar la línea específica
-# contenido['databaseChangeLog'][0]['changeSet']['id'] = sys.argv[2]
-# contenido['databaseChangeLog'][0]['changeSet']['author'] = sys.argv[3]
-# contenido['databaseChangeLog'][0]['changeSet']['changes'][0]['sqlFile']['path'] = sys.argv[4]
+for line in sys.argv[3]:
+    # Imprime cada línea
+    partes = line.split("/")
+    name = partes[-1]
+    archivo_destino = f'{name}_changeset_new.yaml'
+    shutil.copy(archivo_origen, archivo_destino)
 
-# # contenido[sys.argv[2]] = sys.argv[3]
+    archivo_yaml = archivo_destino
 
-# # Escribir el contenido modificado de vuelta al archivo YAML
-# with open(archivo_yaml, 'w') as file:
-#     yaml.dump(contenido, file, default_flow_style=False)
+    # Leer el contenido del archivo YAML
+    with open(archivo_yaml, 'r') as file:
+        contenido = yaml.safe_load(file)
+
+    # Modificar la línea específica
+    contenido['databaseChangeLog'][0]['changeSet']['id'] = sys.argv[1]
+    contenido['databaseChangeLog'][0]['changeSet']['author'] = sys.argv[2]
+    path = f'../{line}'
+    contenido['databaseChangeLog'][0]['changeSet']['changes'][0]['sqlFile']['path'] = path
+    
+    with open(archivo_yaml, 'w') as file:
+        yaml.dump(contenido, file, default_flow_style=False)
